499a1a71423abddfe7fd2d8c2ac66c822661bdf9
StanSain <127401658+StanSain@users.noreply.github.com>
2023-04-19 21:27:09 -0300

    miercoles 19 de abril 2023

--- Dates/src/dates/Dates.java
+++ Dates/src/dates/Dates.java
@@ -1,21 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*amos a usar la clase Date que ya existe en Java. Crearemos la clase
+FechaService, en paquete Servicios, que tenga los siguientes métodos:*/
 package dates;
 
-/**
- *
- * @author John
- */
+import java.text.SimpleDateFormat;
+import servicios.FechaService;
+import java.util.Date;
+
 public class Dates {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-M-dd");
+//        System.out.println(sd.format(fecha));
+//        System.out.println(fecha.getYear() + 1900);
+        FechaService fs = new FechaService();
+        Date cumple = new Date();
+        cumple = fs.Nace();
+        System.out.println("fecha de nacimiento " + sd.format(cumple));
+        System.out.println();
+        System.out.print("hoy es ");
+        System.out.println(sd.format(fs.fechaActual()));
+        fs.diferencia(fs.fechaActual(), cumple);
+
     }
-    
+
 }
---
+++ RecursosHumanos/src/servicios/ServicioPersona.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicios;
+import entidades.Persona;
+import java.util.Date;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+/**
+ *
+ * @author John
+ */
+public class ServicioPersona {
+
+ /*Método crearPersona que pida al usuario Nombre y fecha de
+nacimiento de la persona a crear. Retornar el objeto Persona creado.*/
+    public Persona crear(){
+        Scanner leer = new Scanner(System.in);
+        Persona p1 = new Persona();
+       System.out.println("nombre");
+        p1.setNombre(leer.next());
+        System.out.println("año, mes y dia de nacimiento");
+        int anio = leer.nextInt();
+        int mes = leer.nextInt();
+        int dia = leer.nextInt();
+        Date fecha = new Date(anio-1900,mes-1,dia);
+        p1.setNacimiento(fecha);
+        return p1;
+    }
+}
